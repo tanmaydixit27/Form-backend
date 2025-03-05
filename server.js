773b520d820b3e20087e4e0d8aaa2c3c6f8b0e78
tanmaydixit27 <tanmaydixit7054@gmail.com>
2025-03-05 11:08:33 +0530

    allowed cors for frontend

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "https://formcrudbytanmay.netlify.app", // Allow requests from any frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 app.use("/api/forms", formRoutes);
